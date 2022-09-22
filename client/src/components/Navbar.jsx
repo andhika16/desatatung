c8a7ac73492d0815b5a095286cc1bdb27f3bfabf
andhika16 <moh.andhika16@gmail.com>
2022-09-22 09:43:28 +0700

    new file:   client/.env 	modified:   client/src/App.jsx 	new file:   client/src/assets/ (2).png 	deleted:    client/src/assets/.png 	new file:   client/src/assets/dad.png 	new file:   client/src/assets/down-arrow.png 	modified:   client/src/assets/house.png 	modified:   client/src/assets/loupe.png 	new file:   client/src/assets/telephone-call.png 	new file:   client/src/assets/whatsapp.png 	modified:   client/src/components/Dropdown.jsx 	new file:   client/src/components/Link.js 	modified:   client/src/components/Navbar.jsx 	deleted:    client/src/components/Navcheck.jsx 	modified:   client/src/index.css 	new file:   client/src/routes/Beranda.jsx 	new file:   client/src/routes/Sidebar.jsx 	new file:   package-lock.json 	new file:   package.json

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,86 +4,115 @@ import loupe from "../assets/loupe.png";
 import Dropdown from "./Dropdown";
 import square from "../assets/square.png";
 import { useState } from "react";
-
+import { ProfilDesa, pemerintahan, informasiPublik } from "./Link";
 const Navbar = () => {
   const [showNav, setShowNav] = useState(false);
+  console.log(showNav);
   return (
-    <>
-      <div className=" bg-gray-800">
-        <div className="sm:hidden flex justify-between p-5">
-          <div className="cursor-pointer">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-6 h-6 text-white"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
+    <nav>
+      <div className="w-full  bg-slate-800 flex flex-row py-2 px-10 flex-wrap">
+        <div className="order-1 w-full sm:w-0 ss:hidden  flex flex-row py-2 items-center justify-between ">
+          <div
+            className="order-1 cursor-pointer"
+            onClick={() => setShowNav(!showNav)}
+          >
+            {showNav ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6 text-white"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6 text-white"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            )}
           </div>
-          <div className="w-[25px] h-[25px]">
-            <img src={loupe} alt="loupe" />
+          <div className="sm:block w-[40px] h-[40px] ">
+            <img
+              className="w-full h-full object-contain"
+              src={logoResident}
+              alt="house"
+            />
           </div>
         </div>
-
-        <div className="px-10 py-5 sm:flex items-center justify-between">
-          <div
-            className={`m-3 sm:flex flex-row items-center ${
-              showNav ? "" : "hidden"
-            }`}
-          >
-            <div className="w-[40px] h-[40px] mr-5">
-              <img className="w-full h-full" src={logoResident} alt="house" />
+        <div className="w-full sm:px-10 sm:py-5 flex flex-col justify-between ss:flex-row items-center">
+          <div className="flex flex-row items-center">
+            <div className="hidden ss:block w-[40px] h-[40px] ss:mr-5">
+              <img
+                className="w-full h-full object-contain"
+                src={logoResident}
+                alt="house"
+              />
             </div>
-            <div className="font-inter text-white">
-              <h1 className=" text-lg">Website resmi desa tatung</h1>
+            <div className="font-inter uppercase text-white text-center ss:text-left">
+              <h1 className="text-lg">Website resmi desa tatung</h1>
               <p className="text-sm">Kec.Balong Kab.Ponorogo</p>
             </div>
           </div>
-          <div className="px-4 py-2 hidden sm:flex flex-row items-center bg-white ">
+          <div className="px-4 py-2 my-3 flex flex-row items-center bg-white ">
             <div className="w-[20px] h-[20px]">
-              <img src={loupe} className="w-[100%] h-[100%]" alt="loupe" />
+              <img
+                src={loupe}
+                className="w-[100%] h-[100%] object-contain"
+                alt="loupe"
+              />
             </div>
-            <div className="mx-2">
+            <div className="">
               <input type="search" name="" id="" placeholder="cari" />
             </div>
           </div>
         </div>
       </div>
-      <div className="hidden px-10 sm:flex flex-row  items-center shadow-lg bg-gray-500 uppercase font-semibold flex-wrap">
-        <div className="m-4">
-          <p className="">Beranda</p>
+      <div
+        className={`w-full pr-10  ${
+          showNav ? "" : "hidden"
+        }  ss:flex flex-row  px-10 items-center  flex-wrap font-semibold capitalize`}
+      >
+        <div className="my-4 ss:m-4 ">
+          <p>Beranda</p>
         </div>
-        <Dropdown />
-        <div className="m-4">
-          <p className="">Pemerintahan Desa</p>
+        <Dropdown nama="bpbd" list={pemerintahan} />
+        <div className="my-4 ss:m-4 ">
+          <p>Pemerintahan Desa</p>
         </div>
-        <div className="m-4">
-          <p className="">LEMMAS</p>
+        <Dropdown nama="profil desa" />
+        <div className="my-4 ss:m-4 ">
+          <p>data desa</p>
         </div>
-        <div className="m-4">
-          <p className="">data desa</p>
+        <div className="my-4 ss:m-4 ">
+          <p>ppid</p>
         </div>
-        <div className="m-4">
-          <p className="">ppid</p>
+        <div className="my-4 ss:m-4 ">
+          <p>transparansi keuangan</p>
         </div>
-        <div className="m-4">
-          <p className="">transparansi keuangan</p>
+        <div className="my-4 ss:m-4 ">
+          <p>surat online</p>
         </div>
-        <div className="m-4">
-          <p className="">surat online</p>
-        </div>
-        <div className="m-4">
-          <p className="">login</p>
+        <div className="my-4 ss:m-4 ">
+          <p>login</p>
         </div>
       </div>
-    </>
+      <hr />
+    </nav>
   );
 };
 
